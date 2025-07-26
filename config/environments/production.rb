a7e5602cefbeda24f243d7dbaf641c01affec3f4
solarspin-glitch <solarspin2024@icloud.com>
2025-07-25 19:25:54 -0500

    Create production.rb

---
+++ config/environments/production.rb
@@ -0,0 +1,14 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
+  config.log_level = :info
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.log_formatter = ::Logger::Formatter.new
+end
